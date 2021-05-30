93c9fc2c572865d1c808376aebd2a775cd523afc
Vira-Voitkiv <viravesta@gmail.com>
2021-05-30 14:25:41 +0000

    change

--- js/tabs.js
+++ js/tabs.js
@@ -1,27 +1,21 @@
+let products;
+
 function renderTab(products, category) {
     let html = '';
-    const productsByCategory = products.filter( product => product.category === category );
+    const productsByCategory = products.filter( product => product.category === category);
     for (const product of productsByCategory) {
       html += `
         <article>
           <img src="img/${product.image}" alt="${product.title}">
+          <article class="out-of-stock"><p>>${product.quantity === 0 ? "Out of stock": ""}</p></article>
           <p>${product.title}</p>
           <p>${product.price} USD</p>
-          <p>${product.quantity === 0 ? "OUT OF STOCK": ""}</p> 
         </article>
       `;
     }
     const productsList = document.querySelector('.products-list');
     productsList.innerHTML = html;
   }
-  function fetchProducts() {
-        fetch('productsList.json')
-            .then(response => response.json() )
-            .then(productsFromServer => products = productsFromServer)
-            .then( () => renderTabs() )
-    }
-
-  fetchProducts();
 
   document.querySelector('.tab-armchairs')
           .addEventListener('click', () => {
@@ -32,4 +26,20 @@ function renderTab(products, category) {
   document.querySelector('.tab-tables')
           .addEventListener('click', () => renderTab(products, 'tables'));
   
+  // product.className = 'active';
+  // if (product.quantity === 0) {
+  //   productsByCategory.className = 'active';  ;
+  //   }
+  //   productsByCategory.className = ''
+    
+
+  function fetchProducts() {
+    fetch('productsList.json')
+        .then(response => response.json() )
+        .then(productsFromServer => products = productsFromServer)
+        .then( () => renderTab() )
+}
+    
+      fetchProducts();
+
   renderTab(products, 'armchairs');
