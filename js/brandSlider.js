31cfc37cdde030ade22d4ed6a49fa8e0f50d81c5
Artem <piatnica13@gmail.com>
2021-06-06 17:24:20 +0300

    add adaptive styles

--- js/brandSlider.js
+++ js/brandSlider.js
@@ -12,4 +12,18 @@ const swiper = new Swiper(".mySwiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 5
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 5
+      },
+      640: {
+        slidesPerView: 6,
+        spaceBetween: 5
+      }
+    }
   });
